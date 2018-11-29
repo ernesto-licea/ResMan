6d9574927ac8c673674b11fe4f34faf7b241f465
ernesto <ernesto.licea@gmail.com>
2018-11-28 20:08:43 -0500

    '#001 Create UserFormAdd and UserFormEdit

--- CustomUser/forms.py
+++ CustomUser/forms.py
@@ -2,8 +2,16 @@ from django import forms
 from django.utils.translation import gettext_lazy as _
 
 
+class UserFormBase(forms.ModelForm):
+    class Meta:
+        fields = "__all__"
 
-class UserForm(forms.ModelForm):
+    def clean(self):
+        cleaned_data  = super(UserFormBase, self).clean()
+        return cleaned_data
+
+
+class UserFormAdd(UserFormBase):
     retype_password = forms.CharField(
         label=_('Retype Password'),
         widget=forms.PasswordInput(attrs={'class':'vTextField'}),
@@ -11,14 +19,14 @@ class UserForm(forms.ModelForm):
         help_text=_('Confirm the previous password.')
     )
 
-    class Meta:
+    class Meta(UserFormBase.Meta):
         widgets = {
             'password': forms.PasswordInput(attrs={'class': 'vTextField'}),
             'retype_password': forms.PasswordInput(attrs={'class': 'vTextField'}),
         }
 
     def clean(self):
-        cleaned_data  = super(UserForm, self).clean()
+        cleaned_data  = super(UserFormAdd, self).clean()
         password = cleaned_data.get('password',False)
         retype_password = cleaned_data.get('retype_password',False)
 
@@ -26,4 +34,12 @@ class UserForm(forms.ModelForm):
             if password != retype_password:
                 self.add_error('retype_password',_('Passwords do not match.'))
 
+        return cleaned_data
+
+class UserFormEdit(UserFormBase):
+    class Meta(UserFormBase.Meta):
+        fields = "__all__"
+
+    def clean(self):
+        cleaned_data  = super(UserFormBase, self).clean()
         return cleaned_data
