c3c2751713976377cb2d88805864aa50a2bc086a
ernesto <ernesto.licea@gmail.com>
2018-11-28 20:09:24 -0500

    '#001 Define get_form, get_fieldsets and get_readonly_fields

--- CustomUser/admin.py
+++ CustomUser/admin.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 from django.contrib.auth.hashers import make_password
 from polymorphic.admin import PolymorphicChildModelAdmin, PolymorphicParentModelAdmin, PolymorphicChildModelFilter
 
-from .forms import UserForm
+from .forms import UserFormEdit, UserFormAdd
 from .models import User, UserEnterprise, UserInstitutional, UserGuest
 
 
@@ -17,8 +17,13 @@ class UserAdmin(PolymorphicParentModelAdmin):
 
 class UserAdminBase(PolymorphicChildModelAdmin):
     base_model = User
-    base_form = UserForm
 
+    def get_form(self, request, obj=None, **kwargs):
+        if obj:
+            kwargs['form'] = UserFormEdit
+        else:
+            kwargs['form'] = UserFormAdd
+        return super().get_form(request, obj, **kwargs)
 
     def save_model(self, request, obj, form, change):
         # If user is staff then is superuser too
@@ -35,32 +40,68 @@ class UserAdminBase(PolymorphicChildModelAdmin):
 class UserEnterpriseAdmin(UserAdminBase):
     base_model = UserEnterprise
 
-    fieldsets = (
-        (None, {
-            'fields': ('status', 'is_staff', 'username', 'password', 'retype_password', 'first_name', 'last_name', 'ci_number')
-        }),
-        ('Enterprise Data', {
-            # 'classes': ('collapse',),
-            'fields': ('enterprise_number', 'phone_number', 'extension_number', 'authorized_document'),
-        }),
-        ('Email Service Data', {
-            # 'classes': ('collapse',),
-            'fields': ('email','email_buzon_size','email_message_size','email_domain'),
-        }),
-        ('Internet Service Data', {
-            # 'classes': ('collapse',),
-            'fields': ('proxy_domain','proxy_quota_type','proxy_quota_size','proxy_extra_quota_size'),
-        }),
-        ('FTP Service Data', {
-            # 'classes': ('collapse',),
-            'fields': ('ftp_folder','ftp_size'),
-        }),
-        ('Additional Description', {
-            # 'classes': ('collapse',),
-            'fields': ('note',),
-        }),
-    )
 
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            fielsets = (
+                (None, {
+                    'fields': ('status', 'is_staff', 'username', 'password', 'retype_password', 'first_name', 'last_name', 'ci_number')
+                }),
+                ('Enterprise Data', {
+                    # 'classes': ('collapse',),
+                    'fields': ('enterprise_number', 'phone_number', 'extension_number', 'authorized_document'),
+                }),
+                ('Email Service Data', {
+                    # 'classes': ('collapse',),
+                    'fields': ('email','email_buzon_size','email_message_size','email_domain'),
+                }),
+                ('Internet Service Data', {
+                    # 'classes': ('collapse',),
+                    'fields': ('proxy_domain','proxy_quota_type','proxy_quota_size','proxy_extra_quota_size'),
+                }),
+                ('FTP Service Data', {
+                    # 'classes': ('collapse',),
+                    'fields': ('ftp_folder','ftp_size'),
+                }),
+                ('Additional Description', {
+                    # 'classes': ('collapse',),
+                    'fields': ('note',),
+                }),
+            )
+        else:
+            fielsets = (
+                (None, {
+                    'fields': (
+                    'status', 'is_staff', 'username', 'password', 'first_name', 'last_name',
+                    'ci_number')
+                }),
+                ('Enterprise Data', {
+                    # 'classes': ('collapse',),
+                    'fields': ('enterprise_number', 'phone_number', 'extension_number', 'authorized_document'),
+                }),
+                ('Email Service Data', {
+                    # 'classes': ('collapse',),
+                    'fields': ('email', 'email_buzon_size', 'email_message_size', 'email_domain'),
+                }),
+                ('Internet Service Data', {
+                    # 'classes': ('collapse',),
+                    'fields': ('proxy_domain', 'proxy_quota_type', 'proxy_quota_size', 'proxy_extra_quota_size'),
+                }),
+                ('FTP Service Data', {
+                    # 'classes': ('collapse',),
+                    'fields': ('ftp_folder', 'ftp_size'),
+                }),
+                ('Additional Description', {
+                    # 'classes': ('collapse',),
+                    'fields': ('note',),
+                }),
+            )
+        return fielsets
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ('password',)
+        return self.readonly_fields
 
 class UserInstitutionalAdmin(UserAdminBase):
     base_model = UserInstitutional
