fc70379365bf5c39e0b5b28e5139affa68ead007
ernesto <ernesto.licea@gmail.com>
2018-12-03 02:06:36 -0500

    '#001 create MinimumSymbolAmount password validation class

--- CustomUser/password_validation.py
+++ CustomUser/password_validation.py
@@ -51,3 +51,27 @@ class MinimumUpperLetterAmount:
                 'plural': 's' if self.num_amount > 1 else ""
             }
         )
+class MinimumSymbolAmount:
+    def __init__(self, num_amount=1):
+        self.num_amount = num_amount
+
+    def validate(self, password, user=None):
+        numbers = re.findall('\W', password)
+        if len(numbers) < self.num_amount:
+            raise ValidationError(
+                _("This password must contain at least %(num_amount)d symbol%(plural)s."),
+                code='password_dont_have_symbol',
+                params={
+                    'num_amount': self.num_amount,
+                    'plural': 's' if self.num_amount > 1 else ""
+                },
+            )
+
+    def get_help_text(self):
+        return _(
+            "Your password must contain at least %(num_amount)d symbol%(plural)s."
+            %{
+                'num_amount': self.num_amount,
+                'plural': 's' if self.num_amount > 1 else ""
+            }
+        )
