dda686198bb37f862735cc12655a776c8fc462e3
ernesto <ernesto.licea@gmail.com>
2018-12-03 02:02:19 -0500

    '#001 Create MinimumUpperLetterAmount password validation class

--- CustomUser/password_validation.py
+++ CustomUser/password_validation.py
@@ -27,3 +27,27 @@ class MinimumNumAmount:
                 'plural': 's' if self.num_amount > 1 else ""
             }
         )
+class MinimumUpperLetterAmount:
+    def __init__(self, num_amount=1):
+        self.num_amount = num_amount
+
+    def validate(self, password, user=None):
+        numbers = re.findall('[A-Z]', password)
+        if len(numbers) < self.num_amount:
+            raise ValidationError(
+                _("This password must contain at least %(num_amount)d upper letter%(plural)s."),
+                code='password_dont_have_upper_letter',
+                params={
+                    'num_amount': self.num_amount,
+                    'plural': 's' if self.num_amount > 1 else ""
+                },
+            )
+
+    def get_help_text(self):
+        return _(
+            "Your password must contain at least %(num_amount)d upper letter%(plural)s."
+            %{
+                'num_amount': self.num_amount,
+                'plural': 's' if self.num_amount > 1 else ""
+            }
+        )
