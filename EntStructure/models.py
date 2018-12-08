c762693bc9874ff945cad5c9d0849150eccdcdb6
ernesto <ernesto.licea@gmail.com>
2018-12-08 00:20:25 -0500

    '#002 Create enterprise department model

--- EntStructure/models.py
+++ EntStructure/models.py
@@ -13,3 +13,14 @@ class Area(models.Model):
         db_table = 'enterprise_areas'
         verbose_name = _('enterprise area')
         verbose_name_plural = _('enterprise areas')
+
+class Department(models.Model):
+    area = models.ForeignKey(Area,on_delete=models.CASCADE)
+    name = models.CharField(_('name'), max_length=50, default="", unique=True)
+    responsible = models.CharField(_('responsible'), max_length=100, default="", blank=True)
+    email = models.EmailField(_('email area'))
+
+    class Meta:
+        db_table = 'enterprise_departments'
+        verbose_name = _('enterprise department')
+        verbose_name_plural = _('enterprise departments')
