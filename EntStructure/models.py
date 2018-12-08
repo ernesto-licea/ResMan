3e45749ee756951fd5126e2a9f6efb68c0d83e8d
ernesto <ernesto.licea@gmail.com>
2018-12-08 00:14:25 -0500

    '#002 Create enterprise areas model

--- EntStructure/models.py
+++ EntStructure/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+
+class Area(models.Model):
+    name = models.CharField(_('name'), max_length=50, default="", unique=True)
+    responsible = models.CharField(_('responsible'), max_length=100, default="", blank=True)
+    email = models.EmailField(_('email area'))
+
+    class Meta:
+        db_table = 'enterprise_areas'
+        verbose_name = _('enterprise area')
+        verbose_name_plural = _('enterprise areas')
