bbd8e066f1df4df24bf34c8b0f31bf8ed7dd4349
ernesto <ernesto.licea@gmail.com>
2018-12-09 14:46:15 -0500

    '#003 Create Service model

--- Services/models.py
+++ Services/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(_('name'), max_length=50, default="", unique=True)
+    slugname = models.SlugField(_('slugname'),default="")
+    description = models.TextField(_('description'),blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'enterprise_services'
+        verbose_name = _('enterprise service')
+        verbose_name_plural = _('enterprise services')
