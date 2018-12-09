25c01d951d41200d2b4192db1939aa54b920e4a1
ernesto <ernesto.licea@gmail.com>
2018-12-08 23:16:03 -0500

    '#002 Add field is_active in list_display attribute to be showed in changelist admin view

--- EntStructure/admin.py
+++ EntStructure/admin.py
@@ -5,14 +5,14 @@ from .models import Area, Department
 
 class AreaAdmin(admin.ModelAdmin):
     model = Area
-    list_display = ('name','responsible','email')
+    list_display = ('name','is_active','responsible','email')
 
     def save_model(self, request, obj, form, change):
         super(AreaAdmin,self).save_model(request,obj,form,change)
 
 class DepartmentAdmin(admin.ModelAdmin):
     model = Department
-    list_display = ('name','responsible','email','area')
+    list_display = ('name','is_active','responsible','email','area')
 
     def save_model(self, request, obj, form, change):
         super(DepartmentAdmin,self).save_model(request,obj,form,change)
