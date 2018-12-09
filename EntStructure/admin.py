1f7ec3502d5cf6e11075e7bda5ec0e768eeccdb7
ernesto <ernesto.licea@gmail.com>
2018-12-08 22:52:03 -0500

    '#002 Create admin for Areas and Departments models

--- EntStructure/admin.py
+++ EntStructure/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Area, Department
+
+
+class AreaAdmin(admin.ModelAdmin):
+    model = Area
+    list_display = ('name','responsible','email')
+
+class DepartmentAdmin(admin.ModelAdmin):
+    model = Department
+    list_display = ('name','responsible','email','area')
+
+
+admin.site.register(Area,AreaAdmin)
+admin.site.register(Department,DepartmentAdmin)
