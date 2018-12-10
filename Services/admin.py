b5e1f10585cf7049d285bec5a01a61cee2314cfb
ernesto <ernesto.licea@gmail.com>
2018-12-10 01:09:58 -0500

    '#003 Create admin for Service model

--- Services/admin.py
+++ Services/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.utils.text import slugify
 
-# Register your models here.
+from .models import Service
+
+class ServiceAdmin(admin.ModelAdmin):
+    model = Service
+    fields = ('name','description')
+    list_display = ('name','description')
+
+    def save_model(self, request, obj, form, change):
+        obj.slugname = slugify(obj.name)
+        super(ServiceAdmin,self).save_model(request,obj,form,change)
+
+admin.site.register(Service,ServiceAdmin)
