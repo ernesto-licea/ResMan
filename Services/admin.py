d5fa4c7bd798a6f72da688a1a7f93f3432632dec
ernesto <ernesto.licea@gmail.com>
2018-12-10 14:21:10 -0500

    '#003 Prevent the default services from being deleted or modified
    Create has_delete_permission and has_change_permission to prevent the default services from being deleted or modified

--- Services/admin.py
+++ Services/admin.py
@@ -7,9 +7,22 @@ class ServiceAdmin(admin.ModelAdmin):
     model = Service
     fields = ('name','description')
     list_display = ('name','description')
+    builtin_services = ['internet','email','ftp','chat']
 
     def save_model(self, request, obj, form, change):
         obj.slugname = slugify(obj.name)
         super(ServiceAdmin,self).save_model(request,obj,form,change)
+
+    def has_delete_permission(self, request, obj=None):
+        if obj:
+            if obj.name in self.builtin_services:
+                return False
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        if obj:
+            if obj.name in self.builtin_services:
+                return False
+        return True
 
 admin.site.register(Service,ServiceAdmin)
