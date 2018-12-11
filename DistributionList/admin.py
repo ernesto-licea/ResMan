a25e947069c7fdd7cbad64cb88f8aa6a7464b51a
ernesto <ernesto.licea@gmail.com>
2018-12-11 14:42:53 -0500

    '#004 Create admin for DistributionList model

--- DistributionList/admin.py
+++ DistributionList/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.utils.text import slugify
 
-# Register your models here.
+from .models import DistributionList
+
+
+class DistributionListAdmin(admin.ModelAdmin):
+    model = DistributionList
+    fields = ('name','responsible','email','description')
+    list_display = ('name','responsible','email','description')
+
+    def save_model(self, request, obj, form, change):
+        obj.slugname = slugify(obj.name)
+        super(DistributionListAdmin,self).save_model(request,obj,form,change)
+
+admin.site.register(DistributionList,DistributionListAdmin)
