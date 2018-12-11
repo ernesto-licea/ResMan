5b0a92e8961414467b9b209b7415a152a7cce65d
ernesto <ernesto.licea@gmail.com>
2018-12-11 14:40:00 -0500

    '#004 Add field description to DistributionList model

--- DistributionList/models.py
+++ DistributionList/models.py
@@ -9,6 +9,7 @@ class DistributionList(models.Model):
     responsible = models.CharField(_('responsible'), max_length=100, default="", blank=True)
     email = models.EmailField(_('email area'))
     slugname = models.SlugField(_('slugname'),default="")
+    description = models.TextField(_('description'),blank=True)
 
     def __str__(self):
         return self.name
