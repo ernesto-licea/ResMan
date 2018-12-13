034e09e26ee1aae82819dbf9e383efe7a1696f51
ernesto <ernesto.licea@gmail.com>
2018-12-12 22:45:55 -0500

    '#005 Remove attribute fields and create fieldset to organize add/change form

--- LdapServer/admin.py
+++ LdapServer/admin.py
@@ -5,8 +5,19 @@ from .models import LdapServer
 
 class LdapServerAdmin(admin.ModelAdmin):
     model = LdapServer
-    fields = "__all__"
     list_display = ('name','is_active','socket','bind_dn')
+
+    fieldsets = (
+        (None, {
+            'fields': ('is_active','name','server_host','server_port','bind_dn','bind_password')
+        }),
+        ('Ldap Data Map', {
+            'fields': (
+                'email_domain','email_buzon_size','email_message_size',
+                'proxy_domain','proxy_quota_type','proxy_quota_size','proxy_extra_quota_size'
+            ),
+        }),
+    )
 
     def save_model(self, request, obj, form, change):
         super(LdapServerAdmin,self).save_model(request,obj,form,change)
