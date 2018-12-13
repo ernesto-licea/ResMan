16cd6361293c34668f707b63a789964aacbc62bc
ernesto <ernesto.licea@gmail.com>
2018-12-12 22:34:37 -0500

    '#005 Create admin for LdapServer model

--- LdapServer/admin.py
+++ LdapServer/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import LdapServer
+
+
+class LdapServerAdmin(admin.ModelAdmin):
+    model = LdapServer
+    fields = "__all__"
+    list_display = ('name','is_active','socket','bind_dn')
+
+    def save_model(self, request, obj, form, change):
+        super(LdapServerAdmin,self).save_model(request,obj,form,change)
+
+admin.site.register(LdapServer,LdapServerAdmin)
