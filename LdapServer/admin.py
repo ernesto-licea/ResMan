4c5f80ba63af495fa47dfe35c29b93aa94c49f92
ernesto <ernesto.licea@gmail.com>
2018-12-13 00:04:53 -0500

    '#005 Add action in change list to syncronize database with ldap server
    Create server_action method to show a column in change list view where put action buttons
    
    Create get_urls method to add a new url to request for syncronize database action
    
    Create method sync_data to execute when syncronize database url is requested

--- LdapServer/admin.py
+++ LdapServer/admin.py
@@ -1,11 +1,17 @@
+from django.conf.urls import url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
+
 
 from .models import LdapServer
 
 
 class LdapServerAdmin(admin.ModelAdmin):
     model = LdapServer
-    list_display = ('name','is_active','socket','bind_dn')
+    list_display = ('name','is_active','socket','bind_dn','server_action')
 
     fieldsets = (
         (None, {
@@ -18,6 +24,32 @@ class LdapServerAdmin(admin.ModelAdmin):
             ),
         }),
     )
+
+    def get_urls(self):
+        urls = super(LdapServerAdmin, self).get_urls()
+        custom_urls = [
+            url(
+                r'^(?P<ldap_server_id>.+)/sync/$',
+                self.admin_site.admin_view(self.sync_data),
+                name='sync-server',
+            ),
+        ]
+        return custom_urls + urls
+
+    def server_action(self, obj):
+        return format_html(
+            '<a class="button" href="{}">{}</a>&nbsp;',
+            reverse('admin:sync-server', args=[obj.pk]),
+            _('sync')
+        )
+
+    def sync_data(self,request,ldap_server_id,*args,**kwargs):
+        ldap_server = self.get_object(request,ldap_server_id)
+
+        # Return changelist view
+        url = reverse('admin:%s_%s_changelist' % (self.model._meta.app_label, self.model._meta.model_name))
+        return HttpResponseRedirect(url)
+
 
     def save_model(self, request, obj, form, change):
         super(LdapServerAdmin,self).save_model(request,obj,form,change)
