af2cef09cefac2ffadb0400a833bd65d3610f5f5
ernesto <ernesto.licea@gmail.com>
2018-12-17 01:42:16 -0500

    '#001 Change the implementation of change password in general mixin to change password method per child model

--- CustomUser/admin.py
+++ CustomUser/admin.py
@@ -22,91 +22,89 @@ from .forms import UserFormEdit, UserFormAdd
 from .models import User, UserEnterprise, UserInstitutional, UserGuest, PasswordHistory
 
 
-class UserAdminMixin(admin.ModelAdmin):
-    change_password_form = AdminPasswordChangeForm
-    change_user_password_template = None
+def change_password(self,request, id, form_url=''):
+    if not self.has_change_permission(request):
+        raise PermissionDenied
+    user = self.get_object(request, unquote(id))
+    if user is None:
+        raise Http404(_('%(name)s object with primary key %(key)r does not exist.') % {
+            'name': self.model._meta.verbose_name,
+            'key': escape(id),
+        })
+    if request.method == 'POST':
+        form = AdminPasswordChangeForm(user, request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.password_date = timezone.now()
+            user.ftp_md5_password = hashlib.md5(user._password.encode('utf-8')).hexdigest()
+            user.save()
+
+            change_message = self.construct_change_message(request, form, None)
+            self.log_change(request, user, change_message)
+            msg = gettext('Password changed successfully.')
+            messages.success(request, msg)
+            update_session_auth_hash(request, form.user)
+            return HttpResponseRedirect(
+                reverse(
+                    '%s:%s_%s_change' % (
+                        self.admin_site.name,
+                        user._meta.app_label,
+                        user._meta.model_name,
+                    ),
+                    args=(user.pk,),
+                )
+            )
+    else:
+        form = AdminPasswordChangeForm(user)
+
+    fieldsets = [(None, {'fields': list(form.base_fields)})]
+    adminForm = admin.helpers.AdminForm(form, fieldsets, {})
+
+    context = {
+        'title': _('Change password: %s') % escape(user.get_username()),
+        'adminForm': adminForm,
+        'form_url': form_url,
+        'form': form,
+        'is_popup': (IS_POPUP_VAR in request.POST or
+                     IS_POPUP_VAR in request.GET),
+        'add': True,
+        'change': False,
+        'has_delete_permission': False,
+        'has_change_permission': True,
+        'has_absolute_url': False,
+        'opts': self.model._meta,
+        'original': user,
+        'save_as': False,
+        'show_save': True,
+        **self.admin_site.each_context(request),
+    }
+
+    request.current_app = self.admin_site.name
+
+    return TemplateResponse(
+        request,
+        'admin/auth/user/change_password.html',
+        context,
+    )
+
+class UserAdmin(PolymorphicParentModelAdmin):
+    base_model = User
+    child_models = (UserEnterprise, UserInstitutional, UserGuest)
+    list_filter = (PolymorphicChildModelFilter,)
+    list_display = ('username','status','get_full_name','user_type')
 
     def get_urls(self):
         return [
             path(
                 '<id>/password/',
-                self.admin_site.admin_view(self.user_change_password),
-                name='auth_user_password_change',
+                self.admin_site.admin_view(self.user_password_change),
+                name='user_password_change',
             ),
         ] + super().get_urls()
 
     @sensitive_post_parameters_m
-    def user_change_password(self, request, id, form_url=''):
-        if not self.has_change_permission(request):
-            raise PermissionDenied
-        user = self.get_object(request, unquote(id))
-        if user is None:
-            raise Http404(_('%(name)s object with primary key %(key)r does not exist.') % {
-                'name': self.model._meta.verbose_name,
-                'key': escape(id),
-            })
-        if request.method == 'POST':
-            form = self.change_password_form(user, request.POST)
-            if form.is_valid():
-                user = form.save(commit=False)
-                user.password_date = timezone.now()
-                user.ftp_md5_password = hashlib.md5(user._password.encode('utf-8')).hexdigest()
-                user.save()
-
-                change_message = self.construct_change_message(request, form, None)
-                self.log_change(request, user, change_message)
-                msg = gettext('Password changed successfully.')
-                messages.success(request, msg)
-                update_session_auth_hash(request, form.user)
-                return HttpResponseRedirect(
-                    reverse(
-                        '%s:%s_%s_change' % (
-                            self.admin_site.name,
-                            user._meta.app_label,
-                            user._meta.model_name,
-                        ),
-                        args=(user.pk,),
-                    )
-                )
-        else:
-            form = self.change_password_form(user)
-
-        fieldsets = [(None, {'fields': list(form.base_fields)})]
-        adminForm = admin.helpers.AdminForm(form, fieldsets, {})
-
-        context = {
-            'title': _('Change password: %s') % escape(user.get_username()),
-            'adminForm': adminForm,
-            'form_url': form_url,
-            'form': form,
-            'is_popup': (IS_POPUP_VAR in request.POST or
-                         IS_POPUP_VAR in request.GET),
-            'add': True,
-            'change': False,
-            'has_delete_permission': False,
-            'has_change_permission': True,
-            'has_absolute_url': False,
-            'opts': self.model._meta,
-            'original': user,
-            'save_as': False,
-            'show_save': True,
-            **self.admin_site.each_context(request),
-        }
-
-        request.current_app = self.admin_site.name
-
-        return TemplateResponse(
-            request,
-            self.change_user_password_template or
-            'admin/auth/user/change_password.html',
-            context,
-        )
-
-class UserAdmin(PolymorphicParentModelAdmin,UserAdminMixin):
-    base_model = User
-    child_models = (UserEnterprise, UserInstitutional, UserGuest)
-    list_filter = (PolymorphicChildModelFilter,)
-    list_display = ('username','status','get_full_name','user_type')
+    def user_password_change(self, request, id, form_url=''):
+        return change_password(self,request,id,form_url)
 
 
 class UserAdminBase(PolymorphicChildModelAdmin):
@@ -138,7 +136,7 @@ class UserAdminBase(PolymorphicChildModelAdmin):
         # Create history del new password
         PasswordHistory.objects.create(user=obj, password=obj.password)
 
-class UserEnterpriseAdmin(UserAdminBase,UserAdminMixin):
+class UserEnterpriseAdmin(UserAdminBase):
     base_model = UserEnterprise
     readonly_fields = ('date_joined','last_login','ftp_md5_password','is_active')
     list_display = ('username','status','get_full_name','user_type')
@@ -199,7 +197,20 @@ class UserEnterpriseAdmin(UserAdminBase,UserAdminMixin):
             )
         return fielsets
 
-class UserInstitutionalAdmin(UserAdminBase,UserAdminMixin):
+    def get_urls(self):
+        return [
+            path(
+                '<id>/password/',
+                self.admin_site.admin_view(self.user_enterprise_password_change),
+                name='user_enterprise_password_change',
+            ),
+        ] + super().get_urls()
+
+    @sensitive_post_parameters_m
+    def user_enterprise_password_change(self, request, id, form_url=''):
+        return change_password(self, request, id, form_url)
+
+class UserInstitutionalAdmin(UserAdminBase):
     base_model = UserInstitutional
     readonly_fields = ('date_joined','ftp_md5_password','is_active')
     list_display = ('username','status','get_full_name','user_type')
@@ -253,7 +264,20 @@ class UserInstitutionalAdmin(UserAdminBase,UserAdminMixin):
             )
         return  fieldsets
 
-class UserGuestAdmin(UserAdminBase,UserAdminMixin):
+    def get_urls(self):
+        return [
+            path(
+                '<id>/password/',
+                self.admin_site.admin_view(self.user_institutional_password_change),
+                name='user_institutional_password_change',
+            ),
+        ] + super().get_urls()
+
+    @sensitive_post_parameters_m
+    def user_institutional_password_change(self, request, id, form_url=''):
+        return change_password(self, request, id, form_url)
+
+class UserGuestAdmin(UserAdminBase):
     base_model = UserGuest
     readonly_fields = ('date_joined','ftp_md5_password','is_active')
     list_display = ('username','status','get_full_name','user_type')
@@ -312,6 +336,19 @@ class UserGuestAdmin(UserAdminBase,UserAdminMixin):
                 }),
             )
         return fieldsets
+
+    def get_urls(self):
+        return [
+            path(
+                '<id>/password/',
+                self.admin_site.admin_view(self.user_guest_password_change),
+                name='user_guest_password_change',
+            ),
+        ] + super().get_urls()
+
+    @sensitive_post_parameters_m
+    def user_guest_password_change(self, request, id, form_url=''):
+        return change_password(self, request, id, form_url)
 
 admin.site.register(User,UserAdmin)
 admin.site.register(UserEnterprise,UserEnterpriseAdmin)
