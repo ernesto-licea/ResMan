1fede9542259bbdcc8cfbf4862c60fa06c26b495
ernesto <ernesto.licea@gmail.com>
2018-12-17 16:31:02 -0500

    '#002 Create get_url method in AreaAdmin

--- EntStructure/admin.py
+++ EntStructure/admin.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.utils.text import slugify
 
@@ -8,6 +9,17 @@ class AreaAdmin(admin.ModelAdmin):
     model = Area
     fields = ('is_active','name','responsible','email')
     list_display = ('name','is_active','responsible','email')
+
+    def get_urls(self):
+        urls = super(AreaAdmin, self).get_urls()
+        custom_urls = [
+            url(
+                r'^(?P<ldap_server_id>.+)/sync/$',
+                self.admin_site.admin_view(self.sync_data),
+                name='sync-server',
+            ),
+        ]
+        return custom_urls + urls
 
     def save_model(self, request, obj, form, change):
         obj.slugname = slugify(obj.name)
