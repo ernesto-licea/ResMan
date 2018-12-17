203b4fbb0299c721d29440a68b908eced4248a33
ernesto <ernesto.licea@gmail.com>
2018-12-17 16:34:22 -0500

    '#002 Create sync_data method in AreaAdmin. This method doesn't have function yet

--- EntStructure/admin.py
+++ EntStructure/admin.py
@@ -1,5 +1,10 @@
 from django.conf.urls import url
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.contrib.admin.utils import quote
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.http import urlquote
 from django.utils.text import slugify
 
 from .models import Area, Department
@@ -14,12 +19,33 @@ class AreaAdmin(admin.ModelAdmin):
         urls = super(AreaAdmin, self).get_urls()
         custom_urls = [
             url(
-                r'^(?P<ldap_server_id>.+)/sync/$',
+                r'^(?P<area_id>.+)/sync/$',
                 self.admin_site.admin_view(self.sync_data),
-                name='sync-server',
+                name='sync-area',
             ),
         ]
         return custom_urls + urls
+
+    def sync_data(self,request,area_id,*args,**kwargs):
+        obj = self.get_object(request,area_id)
+        opts = obj._meta
+
+        # Construct message to return
+        obj_url = reverse(
+            'admin:%s_%s_change' % (opts.app_label, opts.model_name),
+            args=(quote(obj.pk),),
+            current_app=self.admin_site.name,
+        )
+        obj_repr = format_html('<a href="{}">{}</a>', urlquote(obj_url), obj)
+        message = format_html(
+            _("Data from area {} was successfully sent to ldap servers."),
+            obj_repr
+        )
+        self.message_user(request, message, messages.SUCCESS)
+
+        # Return changelist view
+        url = reverse('admin:%s_%s_changelist' % (self.model._meta.app_label, self.model._meta.model_name))
+        return HttpResponseRedirect(url)
 
     def save_model(self, request, obj, form, change):
         obj.slugname = slugify(obj.name)
