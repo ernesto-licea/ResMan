bc544899c87f58140597a8985013a1bcb93a4534
ernesto <ernesto.licea@gmail.com>
2018-12-17 16:54:36 -0500

    '#004 Create get_url method in DistributionListAdmin

--- DistributionList/admin.py
+++ DistributionList/admin.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.utils.text import slugify
 
@@ -8,6 +9,17 @@ class DistributionListAdmin(admin.ModelAdmin):
     model = DistributionList
     fields = ('name','responsible','email','description')
     list_display = ('name','responsible','email','description')
+
+    def get_urls(self):
+        urls = super(DistributionListAdmin, self).get_urls()
+        custom_urls = [
+            url(
+                r'^(?P<distributionlist_id>.+)/sync/$',
+                self.admin_site.admin_view(self.sync_data),
+                name='sync-distributionlist',
+            ),
+        ]
+        return custom_urls + urls
 
     def save_model(self, request, obj, form, change):
         obj.slugname = slugify(obj.name)
