6cece510a2baf6eded0f32cad8ff22de5c61e1fe
ernesto <ernesto.licea@gmail.com>
2018-12-26 17:52:57 -0500

    '#005 Fix exception AppRegistryNotReady raised when server is started
    This exception was raised because we can no get LdapServer instance before LdapServer app was instantiated

--- LdapServer/ldap/actions.py
+++ LdapServer/ldap/actions.py
@@ -1,12 +1,12 @@
 from LdapServer.ldap.models import LdapUser, LdapSecurityGroup
 from django.apps import apps
 
-appconfig = apps.get_app_config('LdapServer')
-LdapServer = appconfig.get_model('LdapServer','LdapServer')
 
 
 def create_ldap_user(sender,**kwargs):
     user = kwargs['user']
+    appconfig = apps.get_app_config('LdapServer')
+    LdapServer = appconfig.get_model('LdapServer', 'LdapServer')
     ldap_servers = LdapServer.objects.filter(is_active=True)
     for server in ldap_servers:
         ldap_user = LdapUser(server,user)
@@ -14,6 +14,8 @@ def create_ldap_user(sender,**kwargs):
 
 def create_ldap_security_group(sender,**kwargs):
     service = kwargs['service']
+    appconfig = apps.get_app_config('LdapServer')
+    LdapServer = appconfig.get_model('LdapServer', 'LdapServer')
     ldap_servers = LdapServer.objects.filter(is_active=True)
     for server in ldap_servers:
         ldap_user = LdapSecurityGroup(server,service)
