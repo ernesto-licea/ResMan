b1eb8f0e1d7134be9635f0b00cf92ac38955d679
ernesto <ernesto.licea@gmail.com>
2018-12-26 13:13:48 -0500

    '#005 Get LdapServer model accross appconfig

--- LdapServer/ldap/actions.py
+++ LdapServer/ldap/actions.py
@@ -1,9 +1,10 @@
 from LdapServer.ldap.models import LdapUser
-from LdapServer.models import LdapServer
-
+from django.apps import apps
 
 def create_ldap_user(sender,**kwargs):
     user = kwargs['user']
+    appconfig = apps.get_app_config('LdapServer')
+    LdapServer = appconfig.get_model('LdapServer','LdapServer')
     ldap_servers = LdapServer.objects.filter(is_active=True)
     for server in ldap_servers:
         ldap_user = LdapUser(server,user)
