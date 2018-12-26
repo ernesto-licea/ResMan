b21acbc04126682aea73a0a32fdc034914d3f393
ernesto <ernesto.licea@gmail.com>
2018-12-26 17:43:58 -0500

    '#005 Create handler create_ldap_security_group to be used when create_ldap_service_signal is dispached

--- LdapServer/ldap/actions.py
+++ LdapServer/ldap/actions.py
@@ -1,12 +1,21 @@
-from LdapServer.ldap.models import LdapUser
+from LdapServer.ldap.models import LdapUser, LdapSecurityGroup
 from django.apps import apps
+
+appconfig = apps.get_app_config('LdapServer')
+LdapServer = appconfig.get_model('LdapServer','LdapServer')
+
 
 def create_ldap_user(sender,**kwargs):
     user = kwargs['user']
-    appconfig = apps.get_app_config('LdapServer')
-    LdapServer = appconfig.get_model('LdapServer','LdapServer')
     ldap_servers = LdapServer.objects.filter(is_active=True)
     for server in ldap_servers:
         ldap_user = LdapUser(server,user)
         ldap_user.add_user()
+
+def create_ldap_security_group(sender,**kwargs):
+    service = kwargs['service']
+    ldap_servers = LdapServer.objects.filter(is_active=True)
+    for server in ldap_servers:
+        ldap_user = LdapSecurityGroup(server,service)
+        ldap_user.add()
 
