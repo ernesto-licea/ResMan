88cb3fa80232c7a3b66051f6eab018b16c4a8055
ernesto <ernesto.licea@gmail.com>
2018-12-26 17:45:46 -0500

    '#005 Connect to create_ldap_service_signal and call create_ldap_security_group handler

--- LdapServer/apps.py
+++ LdapServer/apps.py
@@ -1,7 +1,8 @@
 from django.apps import AppConfig
 
 from CustomUser.signals.signals import create_ldap_user_signal
-from LdapServer.ldap.actions import create_ldap_user
+from LdapServer.ldap.actions import create_ldap_user, create_ldap_security_group
+from Services.signals.signals import create_ldap_service_signal
 
 
 class LdapserverConfig(AppConfig):
@@ -10,3 +11,4 @@ class LdapserverConfig(AppConfig):
 
     def ready(self):
         create_ldap_user_signal.connect(create_ldap_user,dispatch_uid='create_ldap_user')
+        create_ldap_service_signal.connect(create_ldap_security_group,dispatch_uid='create_ldap_service')
