0364ee9499dc7520622f767d7ff0f72d35354c09
ernesto <ernesto.licea@gmail.com>
2018-12-27 15:43:01 -0500

    '#005 Create handler create_ldap_distribution_group to be used when create_ldap_distribution_list_signal is dispached

--- LdapServer/ldap/actions.py
+++ LdapServer/ldap/actions.py
@@ -1,4 +1,4 @@
-from LdapServer.ldap.models import LdapUser, LdapSecurityGroup
+from LdapServer.ldap.models import LdapUser, LdapSecurityGroup, LdapDistributionGroup
 from django.apps import apps
 
 
@@ -18,6 +18,15 @@ def create_ldap_security_group(sender,**kwargs):
     LdapServer = appconfig.get_model('LdapServer', 'LdapServer')
     ldap_servers = LdapServer.objects.filter(is_active=True)
     for server in ldap_servers:
-        ldap_user = LdapSecurityGroup(server,service)
-        ldap_user.add()
+        ldap_group = LdapSecurityGroup(server,service)
+        ldap_group.add()
+
+def create_ldap_distribution_group(sender,**kwargs):
+    distribution_list = kwargs['distribution_list']
+    appconfig = apps.get_app_config('LdapServer')
+    LdapServer = appconfig.get_model('LdapServer', 'LdapServer')
+    ldap_servers = LdapServer.objects.filter(is_active=True)
+    for server in ldap_servers:
+        ldap_group = LdapDistributionGroup(server,distribution_list)
+        ldap_group.add()
 
