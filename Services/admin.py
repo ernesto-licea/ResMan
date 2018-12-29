abc2079fa8ea61fc9e76f958c066e05b7f7eceae
ernesto <ernesto.licea@gmail.com>
2018-12-29 01:32:38 -0500

    '#006 Create admin for Service model

--- Services/admin.py
+++ Services/admin.py
@@ -1,3 +1,61 @@
-from django.contrib import admin
+from django.conf.urls import url
+from django.contrib import admin, messages
+from django.contrib.admin.utils import quote
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.http import urlquote
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import Service
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    model = Service
+    list_display = ('name', 'is_active', 'service_type', 'email','description','server_action')
+
+    fields = ['is_active','name', 'service_type', 'email','description']
+
+    def get_urls(self):
+        urls = super(ServiceAdmin, self).get_urls()
+        custom_urls = [
+            url(
+                r'^(?P<service_id>.+)/sync/$',
+                self.admin_site.admin_view(self.sync_data),
+                name='sync-service',
+            ),
+        ]
+        return custom_urls + urls
+
+    def server_action(self, obj):
+        return format_html(
+            '<a class="button" href="{}">{}</a>&nbsp;',
+            reverse('admin:sync-service', args=[obj.pk]),
+            _('sync')
+        )
+
+    def sync_data(self,request,service_id,*args,**kwargs):
+        service = self.get_object(request,service_id)
+        opts = service._meta
+
+        # Construct message to return
+        obj_url = reverse(
+            'admin:%s_%s_change' % (opts.app_label, opts.model_name),
+            args=(quote(service.pk),),
+            current_app=self.admin_site.name,
+        )
+        obj_repr = format_html('<a href="{}">{}</a>', urlquote(obj_url), service)
+        message = format_html(
+            _("Data from service {} was successfully synchronized with ldap servers."),
+            obj_repr
+        )
+        self.message_user(request, message, messages.SUCCESS)
+
+        # Return changelist view
+        url = reverse('admin:%s_%s_changelist' % (self.model._meta.app_label, self.model._meta.model_name))
+        return HttpResponseRedirect(url)
+
+    def save_model(self, request, obj, form, change):
+        super(ServiceAdmin,self).save_model(request,obj,form,change)
+
+admin.site.register(Service,ServiceAdmin)
