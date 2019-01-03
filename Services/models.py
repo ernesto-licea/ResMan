c6b27d2316df8493dd3ba804b8b026e13b62c5ed
ernesto <ernesto.licea@gmail.com>
2019-01-03 12:02:44 -0500

    '#006 Create Service model methods create_ldap_group and modify_ldap_group to dispach create_ldap_group_signal and modify_ldap_group_signal

--- Services/models.py
+++ Services/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from Services.signals import signals
+
+
 class Service(models.Model):
     SERVICE_TYPE = [
         ('security',_('Service')),
@@ -13,6 +16,18 @@ class Service(models.Model):
     email = models.EmailField(_('email'), default="", blank=True)
     description = models.TextField(_('Description'),blank=True)
     slugname = models.SlugField(_('slugname'),blank=True)
+
+    def create_ldap_group(self):
+        signal = getattr(signals, 'create_ldap_group_signal')
+        receivers = signal.send_robust(sender=self.__class__, user=self)
+        for function, error in receivers:
+            return str(error) if error else None
+
+    def modify_ldap_group(self):
+        signal = getattr(signals, 'modify_ldap_group_signal')
+        receivers = signal.send_robust(sender=self.__class__, user=self)
+        for function, error in receivers:
+            return str(error) if error else None
 
     def __str__(self):
         return self.name
