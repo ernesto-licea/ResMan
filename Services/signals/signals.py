6df8f428334eebe7bf0607c4d7606cb78e5a5503
ernesto <ernesto.licea@gmail.com>
2019-01-04 15:46:31 -0500

    '#006 Remove modify_ldap_group_signal and change create_ldap_group_signal for save_ldap_group_signal

--- Services/signals/signals.py
+++ Services/signals/signals.py
@@ -1,4 +1,3 @@
 import django.dispatch
 
-create_ldap_group_signal = django.dispatch.Signal(providing_args=["obj"])
-modify_ldap_group_signal = django.dispatch.Signal(providing_args=["obj"])
+save_ldap_group_signal = django.dispatch.Signal(providing_args=["obj"])
