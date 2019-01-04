4f48e4363c6459220ffc4eec0eba94b75dfbbda6
ernesto <ernesto.licea@gmail.com>
2019-01-04 15:48:49 -0500

    '#005 Change create_ldap_group for save_ldap_group handler

--- LdapServer/ldap/actions.py
+++ LdapServer/ldap/actions.py
@@ -12,7 +12,7 @@ def create_ldap_user(sender,**kwargs):
         ldap_user = LdapUser(server,user)
         ldap_user.add_user()
 
-def create_ldap_group(sender,**kwargs):
+def save_ldap_group(sender,**kwargs):
     service = kwargs['obj']
     appconfig = apps.get_app_config('LdapServer')
     LdapServer = appconfig.get_model('LdapServer', 'LdapServer')
