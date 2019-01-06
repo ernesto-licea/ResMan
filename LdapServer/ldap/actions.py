9d91607787b9647a104f0cbf7209c332c68e0e6e
ernesto <ernesto.licea@gmail.com>
2019-01-06 02:10:38 -0500

    '#005 Catch any exception when handler call save method on ldap user instance, modify message and raise new exception

--- LdapServer/ldap/actions.py
+++ LdapServer/ldap/actions.py
@@ -1,5 +1,11 @@
+from django.contrib.admin.utils import quote
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.http import urlquote
+
 from LdapServer.ldap.models import LdapUser, LdapGroup
 from django.apps import apps
+from django.utils.translation import gettext_lazy as _
 
 
 
@@ -10,7 +16,22 @@ def save_ldap_user(sender,**kwargs):
     ldap_servers = LdapServer.objects.filter(is_active=True)
     for server in ldap_servers:
         ldap_user = LdapUser(server,user)
-        ldap_user.save()
+        try:
+            ldap_user.save()
+        except Exception as e:
+            opts = server._meta
+            # Construct message to return
+            obj_url = reverse(
+                'admin:%s_%s_change' % (opts.app_label, opts.model_name),
+                args=(quote(server.pk),),
+            )
+            obj_repr = format_html('<a href="{}">{}</a>', urlquote(obj_url), server)
+            message = format_html(
+                _("Active Directory {} says: {}."),
+                obj_repr,
+                e
+            )
+            raise Exception(message)
 
 def save_ldap_group(sender,**kwargs):
     service = kwargs['obj']
