9ad7f5b8e6e44f5b9cff349780dddebb3de75f94
ernesto <ernesto.licea@gmail.com>
2019-01-05 23:00:39 -0500

    '#005 Call save method of LdapUser object. Change handler name to save_ldap_user

--- LdapServer/ldap/actions.py
+++ LdapServer/ldap/actions.py
@@ -3,14 +3,14 @@ from django.apps import apps
 
 
 
-def create_ldap_user(sender,**kwargs):
+def save_ldap_user(sender,**kwargs):
     user = kwargs['user']
     appconfig = apps.get_app_config('LdapServer')
     LdapServer = appconfig.get_model('LdapServer', 'LdapServer')
     ldap_servers = LdapServer.objects.filter(is_active=True)
     for server in ldap_servers:
         ldap_user = LdapUser(server,user)
-        ldap_user.add_user()
+        ldap_user.save()
 
 def save_ldap_group(sender,**kwargs):
     service = kwargs['obj']
