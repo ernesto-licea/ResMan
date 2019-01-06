08b4067b75ffe555562c2070fd5d8d3b77515e6e
ernesto <ernesto.licea@gmail.com>
2019-01-05 23:04:12 -0500

    '#001 Change create_ldap_user_signal for save_ldap_user_signal

--- CustomUser/signals/signals.py
+++ CustomUser/signals/signals.py
@@ -1,4 +1,3 @@
 import django.dispatch
 
-create_ldap_user_signal = django.dispatch.Signal(providing_args=["user"])
-modify_ldap_user_signal = django.dispatch.Signal(providing_args=["user"])
+save_ldap_user_signal = django.dispatch.Signal(providing_args=["obj"])
