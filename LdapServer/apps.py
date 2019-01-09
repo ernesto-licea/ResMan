f6efba5d4986917cda87b19cdddc51d9bf442e10
ernesto <ernesto.licea@gmail.com>
2019-01-09 16:36:53 -0500

    '#005 Connect delete_ldap_user_signal with delete_ldap_user handler

--- LdapServer/apps.py
+++ LdapServer/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
-from CustomUser.signals.signals import save_ldap_user_signal
-from LdapServer.ldap.actions import save_ldap_user, save_ldap_group, delete_ldap_group
+from CustomUser.signals.signals import save_ldap_user_signal, delete_ldap_user_signal
+from LdapServer.ldap.actions import save_ldap_user, save_ldap_group, delete_ldap_group, delete_ldap_user
 from Services.signals.signals import save_ldap_group_signal, delete_ldap_group_signal
 
 
@@ -12,4 +12,5 @@ class LdapserverConfig(AppConfig):
     def ready(self):
         save_ldap_user_signal.connect(save_ldap_user,dispatch_uid='save_ldap_user')
         save_ldap_group_signal.connect(save_ldap_group,dispatch_uid='save_ldap_group')
+        delete_ldap_user_signal.connect(delete_ldap_user,dispatch_uid='delete_ldap_user')
         delete_ldap_group_signal.connect(delete_ldap_group,dispatch_uid='delete_ldap_group')
