1079a04c0b0ac66d0447897251b000a4a47dc5a0
ernesto <ernesto.licea@gmail.com>
2019-01-09 16:35:36 -0500

    '#005 Create delete_ldap_user handler

--- LdapServer/ldap/actions.py
+++ LdapServer/ldap/actions.py
@@ -33,6 +33,30 @@ def save_ldap_user(sender,**kwargs):
             )
             raise Exception(message)
 
+def delete_ldap_user(sender,**kwargs):
+    user = kwargs['obj']
+    appconfig = apps.get_app_config('LdapServer')
+    LdapServer = appconfig.get_model('LdapServer', 'LdapServer')
+    ldap_servers = LdapServer.objects.filter(is_active=True)
+    for server in ldap_servers:
+        ldap_user = LdapUser(server, user)
+        try:
+            ldap_user.delete()
+        except Exception as e:
+            opts = server._meta
+            # Construct message to return
+            obj_url = reverse(
+                'admin:%s_%s_change' % (opts.app_label, opts.model_name),
+                args=(quote(server.pk),),
+            )
+            obj_repr = format_html('<a href="{}">{}</a>', urlquote(obj_url), server)
+            message = format_html(
+                _("Active Directory {} says: {}."),
+                obj_repr,
+                e
+            )
+            raise Exception(message)
+
 def save_ldap_group(sender,**kwargs):
     service = kwargs['obj']
     appconfig = apps.get_app_config('LdapServer')
