709b5e4d06ef3bd613bd30a0059404d0bcce7321
ernesto <ernesto.licea@gmail.com>
2019-01-13 18:11:15 -0500

    '#005 Create reset_ldap_user_password handler

--- LdapServer/ldap/actions.py
+++ LdapServer/ldap/actions.py
@@ -57,6 +57,30 @@ def delete_ldap_user(sender,**kwargs):
             )
             raise Exception(message)
 
+def reset_ldap_user_password(sender,**kwargs):
+    user = kwargs['obj']
+    appconfig = apps.get_app_config('LdapServer')
+    LdapServer = appconfig.get_model('LdapServer', 'LdapServer')
+    ldap_servers = LdapServer.objects.filter(is_active=True)
+    for server in ldap_servers:
+        ldap_user = LdapUser(server, user)
+        try:
+            ldap_user.reset_password()
+        except Exception as e:
+            opts = server._meta
+            # Construct message to return
+            obj_url = reverse(
+                'admin:%s_%s_change' % (opts.app_label, opts.model_name),
+                args=(quote(server.pk),),
+            )
+            obj_repr = format_html('<a href="{}">{}</a>', urlquote(obj_url), server)
+            message = format_html(
+                _("Active Directory {} says: {}."),
+                obj_repr,
+                e
+            )
+            raise Exception(message)
+
 def save_ldap_group(sender,**kwargs):
     service = kwargs['obj']
     appconfig = apps.get_app_config('LdapServer')
