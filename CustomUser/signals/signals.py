c66936f4561f3c5a8e4e5478ac72798b5dc0284b
ernesto <ernesto.licea@gmail.com>
2019-01-13 18:13:22 -0500

    '#001 Modify reset_user_password_signal to send obj argument only

--- CustomUser/signals/signals.py
+++ CustomUser/signals/signals.py
@@ -2,4 +2,4 @@ import django.dispatch
 
 save_ldap_user_signal = django.dispatch.Signal(providing_args=["obj"])
 delete_ldap_user_signal = django.dispatch.Signal(providing_args=["obj"])
-reset_user_password_signal = django.dispatch.Signal(providing_args=["obj","password"])
+reset_user_password_signal = django.dispatch.Signal(providing_args=["obj"])
