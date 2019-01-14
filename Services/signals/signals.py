880e7f38e7faf5cf1f704e93d029cd0e7bc2f3a1
ernesto <ernesto.licea@gmail.com>
2019-01-14 16:38:51 -0500

    '#006 Add argument server to be dispatch with save_ldap_group_signal

--- Services/signals/signals.py
+++ Services/signals/signals.py
@@ -1,4 +1,4 @@
 import django.dispatch
 
-save_ldap_group_signal = django.dispatch.Signal(providing_args=["obj"])
+save_ldap_group_signal = django.dispatch.Signal(providing_args=["obj","server"])
 delete_ldap_group_signal = django.dispatch.Signal(providing_args=["obj"])
