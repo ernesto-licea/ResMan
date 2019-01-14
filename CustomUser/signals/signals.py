bb2149481a49a4420f594737a743f791b45088ee
ernesto <ernesto.licea@gmail.com>
2019-01-14 16:47:41 -0500

    '#006 Add argument server to be dispatch with save_ldap_user_signal and with reset_user_password_signal

--- CustomUser/signals/signals.py
+++ CustomUser/signals/signals.py
@@ -1,5 +1,5 @@
 import django.dispatch
 
-save_ldap_user_signal = django.dispatch.Signal(providing_args=["obj"])
+save_ldap_user_signal = django.dispatch.Signal(providing_args=["obj","server"])
 delete_ldap_user_signal = django.dispatch.Signal(providing_args=["obj"])
-reset_user_password_signal = django.dispatch.Signal(providing_args=["obj"])
+reset_user_password_signal = django.dispatch.Signal(providing_args=["obj","server"])
