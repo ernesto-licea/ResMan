72f00d242c37feee6c71d54f9b9f8e9740786fc5
ernesto <ernesto.licea@gmail.com>
2019-07-25 10:46:32 -0400

    Connect auth_ldap_user handler with auth_ldap_user_signal signal

--- LdapServer/apps.py
+++ LdapServer/apps.py
@@ -1,7 +1,9 @@
 from django.apps import AppConfig
 
-from CustomUser.signals.signals import save_ldap_user_signal, delete_ldap_user_signal, reset_user_password_signal
-from LdapServer.ldap.actions import save_ldap_user, save_ldap_group, delete_ldap_group, delete_ldap_user, reset_ldap_user_password
+from CustomUser.signals.signals import save_ldap_user_signal, delete_ldap_user_signal, reset_user_password_signal, \
+    auth_ldap_user_signal
+from LdapServer.ldap.actions import save_ldap_user, save_ldap_group, delete_ldap_group, delete_ldap_user, \
+    reset_ldap_user_password, auth_ldap_user
 from Services.signals.signals import save_ldap_group_signal, delete_ldap_group_signal
 
 
@@ -13,5 +15,6 @@ class LdapserverConfig(AppConfig):
         save_ldap_user_signal.connect(save_ldap_user,dispatch_uid='save_ldap_user')
         save_ldap_group_signal.connect(save_ldap_group,dispatch_uid='save_ldap_group')
         delete_ldap_user_signal.connect(delete_ldap_user,dispatch_uid='delete_ldap_user')
+        auth_ldap_user_signal.connect(auth_ldap_user,dispatch_uid='delete_ldap_user')
         delete_ldap_group_signal.connect(delete_ldap_group,dispatch_uid='delete_ldap_group')
         reset_user_password_signal.connect(reset_ldap_user_password, dispatch_uid='reset_ldap_password')
