553d5d208f78d2b274a9fc1010471ec66f5d9664
ernesto <ernesto.licea@gmail.com>
2019-07-25 09:12:41 -0400

    Add url pointing to logout_view view

--- UserInterface/urls.py
+++ UserInterface/urls.py
@@ -4,4 +4,5 @@ from UserInterface import views
 urlpatterns = [
     # Dashboard main page
     url(r'^$', views.dashboard, name="dashboard"),
+    url(r'^logout/$', views.logout_view, name="logout"),
 ]
