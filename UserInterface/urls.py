f6ca59977191bbe7d719eb3777d773e7314ea1c6
ernesto <ernesto.licea@gmail.com>
2019-07-30 11:09:06 -0400

    Create change_password url pointing to change_password view

--- UserInterface/urls.py
+++ UserInterface/urls.py
@@ -4,5 +4,6 @@ from UserInterface import views
 urlpatterns = [
     # Dashboard main page
     url(r'^$', views.dashboard, name="dashboard"),
+    url(r'^change_password$', views.change_password, name="change_password"),
     url(r'^logout/$', views.logout_view, name="logout"),
 ]
