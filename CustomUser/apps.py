9a6fdd5bfbca80543f63dac56069c8ea5edf7d06
ernesto <ernesto.licea@gmail.com>
2019-08-02 15:57:12 -0400

    Add translation for application verbose_name

--- CustomUser/apps.py
+++ CustomUser/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomuserConfig(AppConfig):
     name = 'CustomUser'
-    verbose_name="User Data"
+    verbose_name=_("User Data")
