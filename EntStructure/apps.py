17ac19bca874b55e5c82f9a1df9fe62610e309d0
ernesto <ernesto.licea@gmail.com>
2019-08-02 16:03:29 -0400

    Add translation for application EntStructure verbose_name

--- EntStructure/apps.py
+++ EntStructure/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class EntstructureConfig(AppConfig):
     name = 'EntStructure'
-    verbose_name="Enterprise Structure"
+    verbose_name=_("Enterprise Structure")
