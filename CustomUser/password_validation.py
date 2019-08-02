c98f29d994650eedcc21d61b0a1353e68038427d
ernesto <ernesto.licea@gmail.com>
2019-08-02 15:41:40 -0400

    Modify messages for translation works

--- CustomUser/password_validation.py
+++ CustomUser/password_validation.py
@@ -15,22 +15,21 @@ class MinimumNumAmount:
         numbers = re.findall('\d', password)
         if len(numbers) < self.num_amount:
             raise ValidationError(
-                _("This password must contain at least %(num_amount)d number%(plural)s."),
-                code='password_dont_have_number',
-                params={
+                _("This password must contain at least %(num_amount)d number%(plural)s.") %{
                     'num_amount': self.num_amount,
                     'plural': 's' if self.num_amount > 1 else ""
                 },
+                code='password_dont_have_number',
+
             )
 
     def get_help_text(self):
         return _(
-            "Your password must contain at least %(num_amount)d number%(plural)s."
-            %{
+            "Your password must contain at least %(num_amount)d number%(plural)s.") %{
                 'num_amount': self.num_amount,
                 'plural': 's' if self.num_amount > 1 else ""
             }
-        )
+
 class MinimumUpperLetterAmount:
     def __init__(self, num_amount=1):
         self.num_amount = num_amount
@@ -39,22 +38,21 @@ class MinimumUpperLetterAmount:
         numbers = re.findall('[A-Z]', password)
         if len(numbers) < self.num_amount:
             raise ValidationError(
-                _("This password must contain at least %(num_amount)d upper letter%(plural)s."),
-                code='password_dont_have_upper_letter',
-                params={
+                _("This password must contain at least %(num_amount)d upper letter%(plural)s.") %{
                     'num_amount': self.num_amount,
                     'plural': 's' if self.num_amount > 1 else ""
                 },
+                code='password_dont_have_upper_letter',
+
             )
 
     def get_help_text(self):
         return _(
-            "Your password must contain at least %(num_amount)d upper letter%(plural)s."
-            %{
+            "Your password must contain at least %(num_amount)d upper letter%(plural)s.") %{
                 'num_amount': self.num_amount,
                 'plural': 's' if self.num_amount > 1 else ""
             }
-        )
+
 class MinimumSymbolAmount:
     def __init__(self, num_amount=1):
         self.num_amount = num_amount
@@ -63,22 +61,20 @@ class MinimumSymbolAmount:
         numbers = re.findall('\W', password)
         if len(numbers) < self.num_amount:
             raise ValidationError(
-                _("This password must contain at least %(num_amount)d symbol%(plural)s."),
-                code='password_dont_have_symbol',
-                params={
+                _("This password must contain at least %(num_amount)d symbol%(plural)s.") %{
                     'num_amount': self.num_amount,
                     'plural': 's' if self.num_amount > 1 else ""
                 },
+                code='password_dont_have_symbol',
+
             )
 
     def get_help_text(self):
         return _(
-            "Your password must contain at least %(num_amount)d symbol%(plural)s."
-            %{
+            "Your password must contain at least %(num_amount)d symbol%(plural)s.") %{
                 'num_amount': self.num_amount,
                 'plural': 's' if self.num_amount > 1 else ""
             }
-        )
 
 class RepeatPasswordAmount:
     def __init__(self, num_amount=1):
@@ -97,12 +93,11 @@ class RepeatPasswordAmount:
             for history in password_history:
                 if check_password(password,history.password):
                     raise ValidationError(
-                        _("This password can not be the current password or the %(num_amount)s password%(plural)s used previously."),
-                        code='password_used',
-                        params={
-                            'num_amount': 'last ' + str(self.num_amount-1) if self.num_amount > 2 else "",
+                        _("This password can not be the current password or the last %(num_amount)s password%(plural)s used previously.") %{
+                            'num_amount': str(self.num_amount-1) if self.num_amount > 2 else "",
                             'plural': 's' if self.num_amount > 2 else ""
                         },
+                        code='password_used',
                     )
 
     def password_changed(self,password, user=None):
@@ -117,9 +112,7 @@ class RepeatPasswordAmount:
 
     def get_help_text(self):
         return _(
-            "Your can not use the current password or the %(num_amount)s password%(plural)s used previously."
-            %{
-                'num_amount': 'last ' + str(self.num_amount-1) if self.num_amount > 2 else "",
+            "Your can not use the current password or the last %(num_amount)s password%(plural)s used previously.") %{
+                'num_amount': str(self.num_amount-1) if self.num_amount > 2 else "",
                 'plural': 's' if self.num_amount > 2 else ""
             }
-        )
