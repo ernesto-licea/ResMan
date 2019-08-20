799104da9a5c44b675b65249e4f164b47fd4ffd4
ernesto <ernesto.licea@gmail.com>
2019-08-20 12:12:56 -0400

    Add validation to name field. Check the name do not exist as enterprise area

--- Services/forms.py
+++ Services/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+
+from EntStructure.models import Area
 
 
 class ServiceForm(forms.ModelForm):
@@ -14,3 +17,13 @@ class ServiceForm(forms.ModelForm):
             if service_type == 'distribution' and email in ['','None']:
                 self.add_error('email',_('If Type of Service is Email Distribution List this field is required.'))
         return cleaned_data
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name',False)
+        if name:
+            try:
+                area = Area.objects.get(name=name)
+                raise ValidationError(_("This name is being used by Enterprise Area"))
+            except Area.DoesNotExist:
+                pass
+        return name
