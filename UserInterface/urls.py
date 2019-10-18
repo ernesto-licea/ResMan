8dce4c210f9e706def126e8c0b2c8cb243493a6e
ernesto <ernesto.licea@gmail.com>
2019-10-18 11:39:53 -0400

    Create url supervision/distribution_list pointing to supervision_distribution view

--- UserInterface/urls.py
+++ UserInterface/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^change_password$', views.change_password, name="change_password"),
     url(r'^supervision/users$', views.supervision_users, name="supervision_users"),
     url(r'^supervision/services$', views.supervision_services, name="supervision_services"),
+    url(r'^supervision/distribution_list$', views.supervision_distribution_list, name="supervision_distribution_list"),
     url(r'^logout/$', views.logout_view, name="logout"),
 ]
