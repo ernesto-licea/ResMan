b1100b7cfe520f0455d312426563b406ec93de1c
ernesto <ernesto.licea@gmail.com>
2019-10-18 11:14:03 -0400

    Create url supervision/services pointing to supervision_services view

--- UserInterface/urls.py
+++ UserInterface/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.dashboard, name="dashboard"),
     url(r'^change_password$', views.change_password, name="change_password"),
     url(r'^supervision/users$', views.supervision_users, name="supervision_users"),
+    url(r'^supervision/services$', views.supervision_services, name="supervision_services"),
     url(r'^logout/$', views.logout_view, name="logout"),
 ]
