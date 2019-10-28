489b6aaeb62a3c6adf07d77606e6944cada23056
ernesto <ernesto.licea@gmail.com>
2019-10-28 15:55:34 -0400

    Configure ExternalDB App verbose name

--- CheckExternalDB/apps.py
+++ CheckExternalDB/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class CheckexternaldbConfig(AppConfig):
     name = 'CheckExternalDB'
+    verbose_name = _('External DBs to check')
