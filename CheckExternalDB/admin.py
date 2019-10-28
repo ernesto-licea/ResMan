45a0614523c999b3a9d8fd1b7d8702f1ce2d7a91
ernesto <ernesto.licea@gmail.com>
2019-10-28 15:58:29 -0400

    Create ExternalDBAdmin to manage model in django administration interface

--- CheckExternalDB/admin.py
+++ CheckExternalDB/admin.py
@@ -1,3 +1,61 @@
-from django.contrib import admin
+import MySQLdb
+import pymssql
+from django.conf.urls import url
+from django.contrib import admin, messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from CustomUser.models import UserEnterprise
+from ResMan.admin import admin_site
+
+from CheckExternalDB.models import ExternalDB
+
+
+class ExternalDBAdmin(admin.ModelAdmin):
+    model = ExternalDB
+    # form = ServiceForm
+    list_display = ('name','is_active','db_type','db_host','db_port','user_action','database_action')
+    list_filter = ('is_active',)
+
+    fields = ['is_active','name','db_type','db_host','db_port','db_name','db_username','db_password','db_query','user_field','user_action','email','description']
+
+
+    def save_model(self, request, obj, form, change):
+        super(ExternalDBAdmin,self).save_model(request,obj,form,change)
+
+    def get_urls(self):
+        urls = super(ExternalDBAdmin, self).get_urls()
+        custom_urls = [
+            url(
+                r'^(?P<external_db_id>.+)/check/$',
+                self.admin_site.admin_view(self.check_users),
+                name='check-users',
+            )
+        ]
+        return custom_urls + urls
+
+    def database_action(self, obj):
+        return format_html(
+            '<a class="button" href="{}">{}</a>&nbsp;',
+            reverse('admin:check-users', args=[obj.pk]),
+            _('Check Users')
+        )
+    database_action.short_description = _("Database Action")
+
+
+    def check_users(self,request,external_db_id,*args,**kwargs):
+        obj = self.get_object(request,external_db_id)
+
+        user_changed = obj.check_users()
+
+        for user in user_changed:
+            self.message_user(request,user['message'], messages.SUCCESS)
+
+        # Return changelist view
+        url = reverse('admin:%s_%s_changelist' % (self.model._meta.app_label, self.model._meta.model_name))
+        return HttpResponseRedirect(url)
+
+
+admin_site.register(ExternalDB,ExternalDBAdmin)
