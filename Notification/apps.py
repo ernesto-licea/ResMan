de71b7dfa74810057e91fa124f28754ed095f033
ernesto <ernesto.licea@gmail.com>
2019-10-31 12:09:49 -0400

    Connect handler to externaldb_check_user_signal signal

--- Notification/apps.py
+++ Notification/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+from CheckExternalDB.signals.signals import externaldb_check_user_signal
+from Notification.signal_actions import notification_externaldb_check_user
 
 
 class NotificationConfig(AppConfig):
     name = 'Notification'
+    verbose_name = _('Email Servers')
+
+
+    def ready(self):
+        externaldb_check_user_signal.connect(notification_externaldb_check_user,dispatch_uid='notification_externaldb_check_user')
