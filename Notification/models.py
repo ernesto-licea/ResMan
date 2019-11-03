da43622f58e08c16269d2bf06dac3080d2fc5e38
ernesto <ernesto.licea@gmail.com>
2019-11-03 11:14:14 -0500

    Change some fields names

--- Notification/models.py
+++ Notification/models.py
@@ -7,11 +7,11 @@ from django.utils.translation import gettext_lazy as _
 class EmailServer(models.Model):
     is_active = models.BooleanField(_('is active'), default=True)
     name = models.CharField(_('name'), max_length=100, default="", unique=True)
-    email_server = models.CharField(_('email server'), max_length=128, default="")
-    email_port = models.PositiveIntegerField(_('email port'), default=25)
+    email_server = models.CharField(_('server name or IP'), max_length=128, default="")
+    email_port = models.PositiveIntegerField(_('server port'), default=25)
     email_username = models.CharField(_('username'),max_length=100,default="")
     email_password = models.CharField(_('password'), max_length=100, default="")
-    use_tls = models.BooleanField(_('Use tls?'),default=False)
+    use_tls = models.BooleanField(_('use tls?'),default=False)
     creation_date = models.DateTimeField(_("Creation Date"),auto_now_add=True)
     modified_date = models.DateTimeField(_("Modified Date"),auto_now=True)
 
