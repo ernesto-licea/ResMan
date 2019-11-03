d1cddbc108c3199783d68934fff63eed418faf86
ernesto <ernesto.licea@gmail.com>
2019-11-03 14:23:58 -0500

    Fix clean for email_password field

--- Notification/forms.py
+++ Notification/forms.py
@@ -83,7 +83,7 @@ class EmailServerFormEdit(EmailServerForm):
         cleaned_data  = super(EmailServerFormEdit, self).clean()
         return cleaned_data
 
-    def clean_password(self):
+    def clean_email_password(self):
         # Regardless of what the user provides, return the initial value.
         # This is done here, rather than on the field, because the
         # field does not have access to the initial value
