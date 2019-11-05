c188e05ebf872c2e7dfb570e4014b931d50eeeb8
ernesto <ernesto.licea@gmail.com>
2019-11-05 11:17:32 -0500

    Create admin view to change database server password

--- CheckExternalDB/admin.py
+++ CheckExternalDB/admin.py
@@ -1,24 +1,89 @@
 import base64
 
-import MySQLdb
-import pymssql
 from django.conf.urls import url
 from django.contrib import admin, messages
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.utils.html import format_html
-from django.utils.translation import gettext_lazy as _
-
-from CheckExternalDB.forms import ExternalDBFormEdit, ExternalDBFormAdd
-from CustomUser.models import UserEnterprise
+from django.contrib.admin.options import IS_POPUP_VAR
+from django.contrib.admin.utils import unquote
+from django.contrib.auth.admin import sensitive_post_parameters_m
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponseRedirect, Http404
+from django.template.response import TemplateResponse
+from django.urls import reverse, path
+from django.utils.html import format_html, escape
+from django.utils.translation import gettext_lazy as _, gettext
+from CheckExternalDB.forms import ExternalDBFormEdit, ExternalDBFormAdd, SetExternalDBPasswordForm
 from ResMan.admin import admin_site
 
 from CheckExternalDB.models import ExternalDB
 
+def change_password(modeladmin,request, id, form_url=''):
+    if not modeladmin.has_change_permission(request):
+        raise PermissionDenied
+    db_server = modeladmin.get_object(request, unquote(id))
+    if db_server is None:
+        raise Http404(_('%(name)s object with primary key %(key)r does not exist.') % {
+            'name': modeladmin.model._meta.verbose_name,
+            'key': escape(id),
+        })
+    if request.method == 'POST':
+        form = SetExternalDBPasswordForm(db_server, request.POST)
+        if form.is_valid():
+
+            db_password = form.cleaned_data.get('password1')
+            db_server = form.save(commit=False)
+            db_server.db_password = base64.b64encode(db_password.encode('utf-8')).decode()
+            db_server.save()
+
+            change_message = modeladmin.construct_change_message(request, form, None)
+            modeladmin.log_change(request, db_server, change_message)
+
+            msg = gettext('Password changed successfully.')
+            messages.success(request, msg)
+
+            return HttpResponseRedirect(
+                reverse(
+                    '%s:%s_%s_changelist' % (
+                        modeladmin.admin_site.name,
+                        db_server._meta.app_label,
+                        db_server._meta.model_name,
+                ))
+            )
+    else:
+        form = SetExternalDBPasswordForm(db_server)
+
+    fieldsets = [(None, {'fields': list(form.base_fields)})]
+    adminForm = admin.helpers.AdminForm(form, fieldsets, {})
+
+    context = {
+        'title': _('Change password: %s') % escape(db_server.name),
+        'adminForm': adminForm,
+        'form_url': form_url,
+        'form': form,
+        'is_popup': (IS_POPUP_VAR in request.POST or
+                     IS_POPUP_VAR in request.GET),
+        'add': True,
+        'change': False,
+        'has_delete_permission': False,
+        'has_change_permission': True,
+        'has_absolute_url': False,
+        'opts': modeladmin.model._meta,
+        'original': db_server,
+        'save_as': False,
+        'show_save': True,
+        **modeladmin.admin_site.each_context(request),
+    }
+
+    request.current_app = modeladmin.admin_site.name
+
+    return TemplateResponse(
+        request,
+        'CheckExternalDB/change_password.html',
+        context,
+    )
+
 
 class ExternalDBAdmin(admin.ModelAdmin):
     model = ExternalDB
-    # form = ServiceForm
     list_display = ('name','is_active','db_type','db_host','db_port','user_action','database_action')
     list_filter = ('is_active',)
 
@@ -41,6 +106,11 @@ class ExternalDBAdmin(admin.ModelAdmin):
     def get_urls(self):
         urls = super(ExternalDBAdmin, self).get_urls()
         custom_urls = [
+            path(
+                '<id>/db_password/',
+                self.admin_site.admin_view(self.server_password_change),
+                name='server_password_change',
+            ),
             url(
                 r'^(?P<external_db_id>.+)/check/$',
                 self.admin_site.admin_view(self.check_users),
@@ -48,6 +118,10 @@ class ExternalDBAdmin(admin.ModelAdmin):
             )
         ]
         return custom_urls + urls
+
+    @sensitive_post_parameters_m
+    def server_password_change(self, request, id, form_url=''):
+        return change_password(self, request, id, form_url)
 
     def database_action(self, obj):
         return format_html(
