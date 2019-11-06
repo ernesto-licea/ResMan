a2742b5cb35aa9f42106d16c16b7ef7f4546e1db
ernesto <ernesto.licea@gmail.com>
2019-11-06 17:38:05 -0500

    Fix bug in command check_users

--- CheckExternalDB/management/commands/check_users.py
+++ CheckExternalDB/management/commands/check_users.py
@@ -13,9 +13,15 @@ class Command(BaseCommand):
 
         try:
             external_db = ExternalDB.objects.get(name=external_name,is_active=True)
-            changed_users = external_db.check_users()
-            for obj in changed_users:
-                self.stdout.write(self.style.SUCCESS("username: %s, message: %s" % (obj['user'].username,obj['message'])))
+            status,changed_users,error = external_db.check_users()
+            if not status:
+                self.stdout.write(self.style.ERROR(error))
+            else:
+                if changed_users:
+                    for obj in changed_users:
+                        self.stdout.write(self.style.SUCCESS("username: %s, message: %s" % (obj['user'].username,obj['message'])))
+                else:
+                    self.stdout.write(self.style.SUCCESS("No user was modified as result of checking agains external database: %s" %external_name))
         except ExternalDB.DoesNotExist:
             self.stdout.write(self.style.ERROR("External Database with name: %s does not exist." %external_name))
 
