7ee28cbc508ed97ea853971be99a527afeb8041c
ernesto <ernesto.licea@gmail.com>
2020-06-08 11:44:35 -0400

    Link password_changed_successfully signal with action to be executed

--- Notification/apps.py
+++ Notification/apps.py
@@ -1,7 +1,8 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 from CheckExternalDB.signals.signals import externaldb_check_user_signal
-from Notification.signal_actions import notification_externaldb_check_user
+from Notification.signal_actions import notification_externaldb_check_user, notification_password_changed_successfully
+from UserInterface.signals.signals import password_changed_successfully
 
 
 class NotificationConfig(AppConfig):
@@ -11,3 +12,4 @@ class NotificationConfig(AppConfig):
 
     def ready(self):
         externaldb_check_user_signal.connect(notification_externaldb_check_user,dispatch_uid='notification_externaldb_check_user')
+        password_changed_successfully.connect(notification_password_changed_successfully,dispatch_uid='notification_password_changed_successfully')
